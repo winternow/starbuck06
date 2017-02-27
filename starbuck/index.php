78349530deda3292f8f16a0db5a6f1e39a3341eb
winternow <dt-namnow@hotmail.com>
2017-02-27 23:34:16 +0700

    Add files via upload

---
+++ starbuck/index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="font-awesome.css">
+  <link rel="stylesheet" type="text/css" href="font-awesome-min.css">
+</head>
+<body>
+  <div class="phone">
+    <div class="firstButton"></div>
+    <div class="button secondButton"></div>
+    <div class="button thirdButton"></div>
+    <div class="button2 fourthButton"></div>  
+    <div class="frame">
+      <div class="topPart">
+        <div class="camera"></div>
+        <div class="speaker"></div>
+      </div>
+      <div class="screen">
+
+          <div class="container" id="scroll-style">
+            <div class="page1">
+                <a href="index.php#page2"><img src="img/page1.jpg" id="img-page1" width="280px"></a>
+            </div><!-- end page1 -->
+
+            <div id="page2">
+              <div id="head-bar"><label>My Cards</label></div><!-- end head-bar -->
+
+              <div id="green-level">
+                <div id="geen-level-1">
+                  <label class="green-title"><i class="fa fa-star" aria-hidden="true"></i>Green Level</label>
+                  <p class="sub-title-green">START YOUR'VE EARNED</p>
+                  <label class="point">101</label>
+                  <p class="dsc-green">Your membership expiry data is 2017-03-39.
+                    <br>49 more Stars to reach Gold level.
+                  </p>
+                </div>
+                <div id="geen-level-2">
+                  <a href="#page3"><button>Rewards</button></a>
+                </div>
+                
+              <div><!-- green-level -->
+              <div id="content-page2">
+                <div id="card-wrap">
+                  <img src="img/card.png">
+                  <a href=""><button class="btn-pay"><i class="fa fa-hand-pointer-o" aria-hidden="true"></i><br>
+Touch to Pay</button></a>
+                </div>
+                <div id="card-info">
+                  <h5>Baht 205.00<span>Registered</span></h5>
+                  <h6>As of 2560-02-22 19:43<span>6080152441596222</span></h6>
+                </div>
+
+                <div id="card-btn">
+                    <button>Manage</button>
+                    <button>Add a Card</button>
+                </div>
+
+                <nav id="nav-bar">
+                  <div id="ul-nav-bar">
+                    <a href="#"><li>HOME</li></a>
+                    <li class="active">CARDS</li>
+                    <a href="#"><li>STORES</li></a>
+                    <a href="#"><li>MESSAGES</li></a>
+                    <a href="#"><li>MORE</li></a>
+                  </div>
+                </nav>
+              </div><!-- end content-page2 -->
+
+            </div><!-- end page2 -->
+
+            <div id="page3">
+              <div id="head-bar"><label><a href="#page2"><i class="fa fa-angle-left" aria-hidden="true"></i></a><span>My Benefits</span></label></div><!-- end head-bar -->
+              <div class="content-page3-1">
+                <div class="page3-1">
+                    <img src="https://www.shareicon.net/download/2016/11/15/852939_coffee_512x512.png" width="30px;">
+                    <p>12 Drinks Purchase Until Your Next Free Tall Drink</p>
+                    <label class="kage"><span class="kage-fill"></span></label>
+                    <p>1/12 (Expires in 2017-05-18)</p>
+                </div>
+                <div class="page3-2">
+                  <select id="filter-select"><option>Filter by:<span>Active</span></option></select>
+                  <p>You are currently eligible to redeem the following reward(s):</p>
+                </div>
+              </div>
+              <div class="content-page3-2">
+                  <img src="https://www.shareicon.net/download/2016/11/15/852939_coffee_512x512.png" width="50px;">
+                  <p>You don't have any earned Rewards at the moment.</p>
+              </div>
+              <div class="content-page3-3">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat sint dolorum blanditiis accusamus quis tenetur expedita cum repellat aliquid ipsa harum mollitia illo veniam </p>
+              </div>
+              
+                <nav id="nav-bar">
+                  <div id="ul-nav-bar">
+
+                    <a href="#"><li>HOME</li></a>
+                    <li class="active">CARDS</li>
+                    <a href="#"><li>STORES</li></a>
+                    <a href="#"><li>MESSAGES</li></a>
+                    <a href="#"><li>MORE</li></a>
+                  </div>
+                </nav>
+              </div><!-- end content-page2 -->
+
+            </div><!-- end page2 -->
+              
+          </div> <!-- end container -->
+          
+
+
+
+
+
+
+
+
+
+      </div>
+      <!-- <div class="topPart">
+        <div class="homeButton"></div>
+      </div> -->
+    </div>
+
+  </div>
+	
+</body>
+</html>
